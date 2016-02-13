a7dee2a5a777f04b836fa2e75be460b65edd6bf1
Ryan Wilson <syndicated.life@gmail.com>
2016-02-13 15:02:42 -0800

    found issue in history api, made comments in app.jsx

--- webpack/application.jsx
+++ webpack/application.jsx
@@ -2,7 +2,11 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory as history } from 'react-router';
+
+import Router from 'react-router/lib/Router';
+import Route from 'react-router/lib/Route';
+import IndexRoute from 'react-router/lib/IndexRoute';
+
 import { Provider } from 'react-redux';
 import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
@@ -11,6 +15,19 @@ import thunk from 'redux-thunk';
 import { syncHistory, routeReducer } from 'react-router-redux';
 
 import reducers from './reducers';
+
+// hard refresh works but has warning
+// import createHistory from 'history/lib/createHashHistory';
+// const history = createHistory();
+a
+// hard refresh breaks
+// import useRouterHistory from 'react-router/lib/useRouterHistory';
+// import createHashHistory from 'history/lib/createHashHistory';
+// const history = useRouterHistory(createHashHistory)();
+
+// hard refresh breaks
+import hashHistory from 'react-router/lib/hashHistory';
+const history = hashHistory;
 
 const logger = createLogger();
 const middleware = syncHistory(history);
@@ -47,29 +64,30 @@ import AppThanks from './views/Thanks';
 import AppVerify from './views/Verify';
 // USER ROUTES
 import UserChangePassword from './views/User/ChangePassword';
+// ERROR ROUTES
+import NoMatch from './views/NoMatch';
 
 import './style.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>
-      <Router history={history}>
-        <Route path="/" component={App}>
-          <IndexRoute component={AppHome} />
-          <Route path="about" component={AppAbout} />
-          <Route path="login" component={AppLogin} />
-          <Route path="lostPassword" component={AppLostPassword} />
-          <Route path="register" component={AppRegister} />
-          <Route path="reset" component={AppReset} />
-          <Route path="thanks" component={AppThanks} />
-          <Route path="verify" component={AppVerify} />
-        </Route>
-        <Route path="/user" component={User}>
-          <IndexRoute component={UserHome} />
-          <Route path="changePassword" component={UserChangePassword} />
-        </Route>
-      </Router>
-    </div>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={AppHome} />
+        <Route path="about" component={AppAbout} />
+        <Route path="login" component={AppLogin} />
+        <Route path="lostPassword" component={AppLostPassword} />
+        <Route path="register" component={AppRegister} />
+        <Route path="reset" component={AppReset} />
+        <Route path="thanks" component={AppThanks} />
+        <Route path="verify" component={AppVerify} />
+      </Route>
+      <Route path="/user" component={User}>
+        <IndexRoute component={UserHome} />
+        <Route path="changePassword" component={UserChangePassword} />
+      </Route>
+      <Route path="*" component={NoMatch} />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
---
+++ webpack/views/NoMatch/index.jsx
@@ -0,0 +1,56 @@
+import React, { PropTypes } from 'react';
+import { Button } from 'react-bootstrap';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { routeActions } from 'react-router-redux';
+
+import './style.css';
+
+class NoMatch extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    store: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired
+  };
+  render = () => {
+    const { push } = this.props;
+    return (
+      <section id="no-match">
+        <div className="container">
+          <h1>404</h1>
+          <Button
+            bsClass="btn"
+            bsStyle="primary"
+            onClick={() => push('/')}>
+            ★GO HOME★
+          </Button>
+        </div>
+      </section>
+    );
+  };
+}
+
+const mapStateToProps = (state) => {
+  const { store } = state;
+  const { data, session } = store;
+  const { isAuthenticated } = session;
+  return {
+    isAuthenticated,
+    isFetching: data.isFetching || session.isFetching,
+    store
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch,
+    push: bindActionCreators(routeActions.push, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NoMatch);
